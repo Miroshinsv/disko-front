91d33e727b0be5f68cc3eeb61a0f0508bae23236
miroshinsergey <msv@forward.lc>
2021-07-04 20:55:44 +0300

    add ternary

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ function Card({ name, img, isActive, onEditShedulerCardClick, dataSchedele, onSc
         <div className="card">
           <img className="card__img" src={img}/>
             <h2 className="card__title">Название дискотеки: {name} </h2>
-            <p>Активна/Не активна {isActive}</p>
+            <p>{isActive? "Активна" : "Не активна"}</p>
             <button className="card__btn-active"/>
             <button className="card__btn-edit" onClick={clickEdit}/>
             <button className="card__btn-delete"/>
