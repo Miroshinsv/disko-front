b65fb9fd6991020c92f0251bb9ca0bed5e046858
Miroshin Sergey <baloo@wisebits.com>
2021-10-16 19:59:32 +0300

    refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,18 @@ import Card from "./Card";
 import SchedulerCard from "./schedule/SchedulerCard";
 import ScheduleDay from "./schedule/ScheduleDay";
 
-function Main({ addDaysCardPopupClick, addCardPopupClik, editSchedulePopupClick, onEditShedulerCardClick, onLogout, onCardActiveClick, onCardDelete, schedule }) {
+function Main({
+                // addDaysCardPopupClick,
+                addCardPopupClik,
+                editSchedulePopupClick,
+                onEditShedulerCardClick,
+                onLogout,
+                onCardActiveClick,
+                onCardDelete,
+                schedule
+              }) {
 
-  const weeks= {
+  const weeks = {
     monday: [],
     tuesday: [],
     wednesday: [],
@@ -31,140 +40,17 @@ function Main({ addDaysCardPopupClick, addCardPopupClik, editSchedulePopupClick,
     } else {
       weeks[undefined].push(to);
     }
+    return (
+      <>
+        <Header linkTitle="" path='/sign-in' onLogout={onLogout}/>
+        <main className="content block-size">
+          <button className="" onClick={addCardPopupClik}>Добавить дискотеку</button>
+          <Accordion/>
+          <ScheduleDay events={weeks}/>
+        </main>
+      </>
+    );
   }
-
-  function renderSections() {
-    for(let day of weeks) {
-      ScheduleDay(day, weeks[day])
-    }
-
-    console.log('Функция запущена');
-  }
-
-  return (
-    <>
-      <Header linkTitle="" path='/sign-in' onLogout={onLogout}/>
-      <main className="content block-size">
-        <button className="" onClick={addCardPopupClik}>Добавить дискотеку</button>
-
-        <Accordion />
-        {renderSections}
-
-        <section className="week-day">
-          <h2 className="week-day__title" onClick={addDaysCardPopupClick}>Вторник</h2>
-          <div className="container">
-            {
-              weeks['tuesday'].map(item =>
-                <Card key={item.value.ID}
-                      name={item.value.name}
-                      img={item.value.logo}
-                      isActive={item.value.is_active}
-                      onScheduleClick={editSchedulePopupClick}
-                      onEditShedulerCardClick={onEditShedulerCardClick}
-                      dataSchedele={item.value}
-                      onCardActiveClick={onCardActiveClick}
-                      onCardDelete={onCardDelete}
-                />)
-            }
-          </div>
-        </section>
-
-        <section className="week-day">
-          <h2 className="week-day__title" onClick={addDaysCardPopupClick}>Среда</h2>
-          <div className="container">
-            {
-              weeks['wednesday'].map(item =>
-                <Card key={item.value.ID}
-                      name={item.value.name}
-                      img={item.value.logo}
-                      isActive={item.value.is_active}
-                      onScheduleClick={editSchedulePopupClick}
-                      onEditShedulerCardClick={onEditShedulerCardClick}
-                      dataSchedele={item.value}
-                      onCardActiveClick={onCardActiveClick}
-                      onCardDelete={onCardDelete}
-                />)
-            }
-          </div>
-        </section>
-
-        <section className="week-day">
-          <h2 className="week-day__title" onClick={addDaysCardPopupClick}>Четверг</h2>
-          <div className="container">
-            {
-              weeks['thursday'].map(item =>
-                <Card key={item.value.ID}
-                      name={item.value.name}
-                      img={item.value.logo}
-                      isActive={item.value.is_active}
-                      onScheduleClick={editSchedulePopupClick}
-                      onEditShedulerCardClick={onEditShedulerCardClick}
-                      dataSchedele={item.value}
-                      onCardActiveClick={onCardActiveClick}
-                      onCardDelete={onCardDelete}
-                />)
-            }
-          </div>
-        </section>
-
-        <section className="week-day">
-          <h2 className="week-day__title" onClick={addDaysCardPopupClick}>Пятница</h2>
-          <div className="container">
-            {
-              weeks['friday'].map(item =>
-                <Card key={item.value.ID}
-                      name={item.value.name}
-                      img={item.value.logo}
-                      isActive={item.value.is_active}
-                      onScheduleClick={editSchedulePopupClick}
-                      onEditShedulerCardClick={onEditShedulerCardClick}
-                      dataSchedele={item.value}
-                      onCardActiveClick={onCardActiveClick}
-                      onCardDelete={onCardDelete}
-                />)
-            }
-          </div>
-        </section>
-
-        <section className="week-day">
-          <h2 className="week-day__title" onClick={addDaysCardPopupClick}>Суббота</h2>
-          <div className="container">
-            {
-              weeks['saturday'].map(item =>
-                <Card key={item.value.ID}
-                      name={item.value.name}
-                      img={item.value.logo}
-                      isActive={item.value.is_active}
-                      onScheduleClick={editSchedulePopupClick}
-                      onEditShedulerCardClick={onEditShedulerCardClick}
-                      dataSchedele={item.value}
-                      onCardActiveClick={onCardActiveClick}
-                      onCardDelete={onCardDelete}
-                />)
-            }
-          </div>
-        </section>
-
-        <section className="week-day">
-          <h2 className="week-day__title" onClick={addDaysCardPopupClick}>Воскресенье</h2>
-          <div className="container">
-            {
-              weeks['sunday'].map(item => <Card
-                key={item.value.ID}
-                name={item.value.name}
-                img={item.value.logo}
-                isActive={item.value.is_active}
-                onScheduleClick={editSchedulePopupClick}
-                onEditShedulerCardClick={onEditShedulerCardClick}
-                dataSchedele={item.value}
-                onCardActiveClick={onCardActiveClick}
-                onCardDelete={onCardDelete}
-              />)
-            }
-          </div>
-        </section>
-      </main>
-    </>
-  );
 }
+
 export default Main;
--- src/components/schedule/ScheduleDay.js
+++ src/components/schedule/ScheduleDay.js
@@ -1,20 +1,21 @@
-import React from "react";
-import SchedulerCard from "./SchedulerCard";
+import React, {Component} from "react";
 
-function ScheduleDay(weekDay, events) {
-  // onClick={addDaysCardPopupClick}
-  return(
-    <section className="week-day">
-      <h2 className="week-day__title" >{weekDay}</h2>
-      <div className="container">
-        {
-          events.map(item =>
-            <SchedulerCard item={item}/>
-          )
-        }
-      </div>
-    </section>
-  );
+class ScheduleDay extends Component {
+  events;
+
+  constructor({context, events}) {
+    super(context);
+    this.events = events;
+  }
+
+  render() {
+    return (
+      this.events.forEach(
+        val => <div>{val}</div>
+      )
+    )
+  }
 }
+
 
 export default ScheduleDay;
